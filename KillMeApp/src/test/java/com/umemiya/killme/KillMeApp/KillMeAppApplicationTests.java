edd54484c0a9a2ad1851181960754777e8c24bc6
梅宮 <euthanasia045@gmail.com>
2020-05-23 16:57:12 +0900

    init project

---
+++ KillMeApp/src/test/java/com/umemiya/killme/KillMeApp/KillMeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umemiya.killme.KillMeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillMeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
