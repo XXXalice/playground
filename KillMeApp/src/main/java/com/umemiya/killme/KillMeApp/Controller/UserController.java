2103942b2656e5e624720dc24805e6ecb3205aac
梅宮 <euthanasia045@gmail.com>
2020-05-26 00:05:53 +0900

    uri param test

--- KillMeApp/src/main/java/com/umemiya/killme/KillMeApp/Controller/UserController.java
+++ KillMeApp/src/main/java/com/umemiya/killme/KillMeApp/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.umemiya.killme.KillMeApp.Controller;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,16 @@ public class UserController {
     @RequestMapping("/")
     public String index() {
         return "Hello.";
+    }
+
+    @RequestMapping("/{num}")
+    public String numTest(
+            @PathVariable int num
+    ) {
+        int result = 0;
+        for (int i = 1; i <= num; i++) {
+            result += i;
+        }
+        return "total: " + String.valueOf(result);
     }
 }
