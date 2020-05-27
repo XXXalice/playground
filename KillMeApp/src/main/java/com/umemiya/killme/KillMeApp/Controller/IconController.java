48e1ee2bbe77c5ddb9f9c9927dd98184d1247c95
梅宮 <euthanasia045@gmail.com>
2020-05-27 23:57:03 +0900

    add iconController

--- KillMeApp/src/main/java/com/umemiya/killme/KillMeApp/Controller/IconController.java
+++ KillMeApp/src/main/java/com/umemiya/killme/KillMeApp/Controller/IconController.java
@@ -1,4 +1,14 @@
 package com.umemiya.killme.KillMeApp.Controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IconController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
