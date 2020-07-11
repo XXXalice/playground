e20ffebc0d95b91a6dac22e5e652f4789ae3da3e
梅宮 <euthanasia045@gmail.com>
2020-07-12 02:57:52 +0900

    RandomWordMakerの実装

--- IoPractice/src/com/umemiya/bufferpractice/BufferPractice.java
+++ IoPractice/src/com/umemiya/bufferpractice/BufferPractice.java
@@ -1,10 +1,20 @@
 package com.umemiya.bufferpractice;
 
 public class BufferPractice {
+
+    /**
+     * 連結テスト
+     */
+    private static StringBuffer superStr = new StringBuffer();
+
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer();
         String str = sb.append("こんにちは").append("私の").append("名前は").append("梅宮です").toString();
         System.out.println(str);
 
+        String str2 = sb.append("a").toString();
+        System.out.println(str2);
+
     }
+
 }
---
+++ IoPractice/src/com/umemiya/bufferpractice/RandomWordMaker.java
@@ -0,0 +1,56 @@
+package com.umemiya.bufferpractice;
+
+import java.util.Random;
+
+/**
+ * todo: コンストラクタから文字数を変更できるようにしたい
+ * ランダムなトークン文字列（数値のみ）を生成するクラス
+ *
+ * @author umemiya
+ */
+public class RandomWordMaker {
+
+    /**
+     * 文字数
+     */
+    private static int tokenLength;
+
+    /**
+     * nextInt()で使用する乱数範囲（必ず1桁になる）
+     */
+    private static final int MAKE_RANGE = 10;
+
+    /**
+     * 5文字固定
+     * メモリ領域確保
+     */
+    private static String[] wordMem = new String[5];
+
+    /**
+     * ========コンストラクタ========
+     * todo:　上に則る
+     *
+     * @param tokenLength
+     */
+    public RandomWordMaker(int tokenLength) {
+        this.tokenLength = tokenLength;
+    }
+
+    /**
+     * static
+     *
+     * token生成メソッド
+     *
+     *
+     * @return token文字列
+     */
+    public static String makeToken() {
+        Random rnd = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < wordMem.length; i++) {
+            sb.append(rnd.nextInt(MAKE_RANGE));
+        }
+        return sb.toString();
+    }
+
+}
