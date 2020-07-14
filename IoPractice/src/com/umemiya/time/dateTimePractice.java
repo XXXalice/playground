54fcdb0eead02090f3fa42c98b76e98220462503
梅宮 <euthanasia045@gmail.com>
2020-07-14 21:19:52 +0900

    add pattern

--- IoPractice/src/com/umemiya/time/dateTimePractice.java
+++ IoPractice/src/com/umemiya/time/dateTimePractice.java
@@ -13,5 +13,6 @@ public class dateTimePractice {
         System.out.println(d.format(f));
         //now()で現在の年、月、日がわかる
         System.out.println(LocalDate.now());
+        LocalDate d2 = LocalDate.parse(LocalDate.now().toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 }
