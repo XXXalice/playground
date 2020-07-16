f378ae75944fcb786c818a6b83cc52ea0e7d4dc8
梅宮 <euthanasia045@gmail.com>
2020-07-16 23:35:45 +0900

    いらないimportを削除

--- IoPractice/src/com/umemiya/collectionprc/MapPrc.java
+++ IoPractice/src/com/umemiya/collectionprc/MapPrc.java
@@ -1,9 +1,7 @@
 package com.umemiya.collectionprc;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class MapPrc implements Fruits{
 
