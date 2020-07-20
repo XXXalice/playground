4f00ddbc8146d4a9c207fec341c971ca4e2ad6b2
梅宮 <euthanasia045@gmail.com>
2020-07-21 01:30:42 +0900

    animalListの定義

--- IoPractice/src/com/umemiya/collectionprc/Sansyo.java
+++ IoPractice/src/com/umemiya/collectionprc/Sansyo.java
@@ -1,5 +1,8 @@
 package com.umemiya.collectionprc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Animal {
     public String name;
     public int age;
@@ -7,6 +10,10 @@ class Animal {
 
 public class Sansyo {
     public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.name = "cat";
+        animal.age = 10;
 
+        List<Animal> animalList = new ArrayList<>();
     }
 }
