d49bbcf79c9b46db7bc6c8cea4cb6b02eaed70d8
梅宮 <euthanasia045@gmail.com>
2020-07-21 01:27:06 +0900

    参照の追加

---
+++ IoPractice/src/com/umemiya/collectionprc/Sansyo.java
@@ -0,0 +1,12 @@
+package com.umemiya.collectionprc;
+
+class Animal {
+    public String name;
+    public int age;
+}
+
+public class Sansyo {
+    public static void main(String[] args) {
+
+    }
+}
