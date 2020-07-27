74ee016b9e2243e4bb95df43678e3a75d5a27e72
梅宮 <euthanasia045@gmail.com>
2020-07-27 14:27:28 +0900

    add Cloneable

---
+++ IoPractice/src/com/umemiya/collectionprc/Apple.java
@@ -0,0 +1,10 @@
+package com.umemiya.collectionprc;
+
+public class Apple implements Cloneable {
+    private static String name;
+    private static int id;
+
+    public Apple clone(){
+
+    }
+}
