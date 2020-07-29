f3dc875766edffe09c5b610c95b1ad35544f35a7
梅宮 <euthanasia045@gmail.com>
2020-07-29 18:15:25 +0900

    build

--- IoPractice/src/com/umemiya/janken/JankenApplication.java
+++ IoPractice/src/com/umemiya/janken/JankenApplication.java
@@ -1,7 +1,10 @@
 package com.umemiya.janken;
 
+// import static宣言
+import static com.umemiya.janken.Hands.*;
+
 public class JankenApplication {
     public static void main(String[] args) {
-
+        System.out.println("a");
     }
 }
