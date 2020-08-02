c93d14cce3ef80f16333659f61358ad8a118a2bf
梅宮 <euthanasia045@gmail.com>
2020-08-03 02:10:05 +0900

    add janken game

--- IoPractice/src/com/umemiya/janken/JankenApplication.java
+++ IoPractice/src/com/umemiya/janken/JankenApplication.java
@@ -6,8 +6,11 @@ import com.umemiya.janken.handler.JudgeHandler;
 import static com.umemiya.janken.Hands.*;
 
 public class JankenApplication {
+
     public static void main(String[] args) {
         JudgeHandler judgeHandler = new JudgeHandler();
+        Hands pHands = PAPER;
+        Hands cHands = ROCK;
 
     }
 }
