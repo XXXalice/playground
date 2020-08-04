017b89e5f93070582ebacc7afdda9ec0e54f4faf
梅宮 <euthanasia045@gmail.com>
2020-08-04 22:25:44 +0900

    インナーメソッドの定義

--- IoPractice/src/com/umemiya/classPrc/Outer2.java
+++ IoPractice/src/com/umemiya/classPrc/Outer2.java
@@ -1,11 +1,18 @@
 package com.umemiya.classPrc;
 
 public class Outer2 {
+    // 非finalメンバ
     int outerMember;
     void outerMethod() {
+        // 非finalローカル変数
         int a = 10;
         class Inner {
-
+            public void innerMethod() {
+                System.out.println("innerMethodです");
+                System.out.println(outerMember);
+            }
         }
+        Inner ic = new Inner();
+        ic.innerMethod();
     }
 }
