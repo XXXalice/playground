ba4d6d78680ee22ab2ab9c7f28365d4f1289f45e
梅宮 <euthanasia045@gmail.com>
2020-08-04 23:02:51 +0900

    add comment

--- IoPractice/src/com/umemiya/classPrc/AnnonClass.java
+++ IoPractice/src/com/umemiya/classPrc/AnnonClass.java
@@ -4,6 +4,7 @@ public class AnnonClass {
     public static void main(String[] args) {
         Pocket<Object> pocket = new Pocket<>();
         System.out.println("匿名の使い捨てクラス");
+        // 匿名クラスは宣言と利用を同時に行う
         pocket.setItem(new Object() {
             String innerField;
             void innerMethod() {
