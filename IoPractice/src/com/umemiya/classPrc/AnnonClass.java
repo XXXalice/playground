612bff781cb7ea69aab24f09512b1c1540dec733
梅宮 <euthanasia045@gmail.com>
2020-08-04 22:59:06 +0900

    匿名クラスの宣言

--- IoPractice/src/com/umemiya/classPrc/AnnonClass.java
+++ IoPractice/src/com/umemiya/classPrc/AnnonClass.java
@@ -2,7 +2,13 @@ package com.umemiya.classPrc;
 
 public class AnnonClass {
     public static void main(String[] args) {
-        Pocket<String> pocket = new Pocket<>();
+        Pocket<Object> pocket = new Pocket<>();
         System.out.println("匿名の使い捨てクラス");
+        pocket.setItem(new Object() {
+            String innerField;
+            void innerMethod() {
+                System.out.println(innerField);
+            }
+        });
     }
 }
