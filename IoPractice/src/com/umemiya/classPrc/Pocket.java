e3ad04200544bf0652b64fb2e7cbf7a78ad08d25
梅宮 <euthanasia045@gmail.com>
2020-08-08 14:46:37 +0900

    各鍵の必要解錠回数

--- IoPractice/src/com/umemiya/classPrc/Pocket.java
+++ IoPractice/src/com/umemiya/classPrc/Pocket.java
@@ -1,9 +1,34 @@
 package com.umemiya.classPrc;
 
+import com.umemiya.box.KeyType;
+
 public class Pocket<E> {
+    /**
+     * その他の型
+     */
     private E item;
+    /**
+     * キータイプ
+     */
+    private KeyType keyType;
+    /**
+     * キー解除のカウント
+     */
+    private long count = 0;
+
+    private final long PADLOCK_COUNT = 1024;
+    private final long BUTTON = 10000;
+    private final long DIAL = 30000;
+    private final long FINGER = 1000000;
+
+
+    public Pocket(KeyType keyType) {
+        this.keyType = keyType;
+    }
 
     public E getItem() {
+        this.count++;
+
         return item;
     }
 
