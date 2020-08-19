061843cb1b01f9ace99aa12cac96ec78fa4a96d2
梅宮 <euthanasia045@gmail.com>
2020-08-19 10:30:16 +0900

    add func

--- IoPractice/src/com/umemiya/lambdaPrc/LambdaOut.java
+++ IoPractice/src/com/umemiya/lambdaPrc/LambdaOut.java
@@ -1,4 +1,13 @@
 package com.umemiya.lambdaPrc;
 
+import java.util.function.IntToDoubleFunction;
+
 public class LambdaOut {
+    public static void main(String[] args) {
+        double b = 3.14;
+
+        IntToDoubleFunction getCircle = (x) -> {
+            return x * x * b;
+        };
+    }
 }
