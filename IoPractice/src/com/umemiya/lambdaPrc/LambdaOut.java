597f85f796bb3ef29a8cfe3c59c9d5c50a2be4ba
梅宮 <euthanasia045@gmail.com>
2020-08-19 10:31:53 +0900

    add comment

--- IoPractice/src/com/umemiya/lambdaPrc/LambdaOut.java
+++ IoPractice/src/com/umemiya/lambdaPrc/LambdaOut.java
@@ -4,6 +4,8 @@ import java.util.function.IntToDoubleFunction;
 
 public class LambdaOut {
     public static void main(String[] args) {
+        // bは実質的にfinalな変数である必要がある
+        // ラムダ内部で書き換えることができない
         double b = 3.14;
 
         IntToDoubleFunction getCircle = (x) -> {
