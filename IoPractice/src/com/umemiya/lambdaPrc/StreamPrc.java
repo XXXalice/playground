f8f4564ce42ec25a8ee1db8da288ef1d829d68ba
梅宮 <euthanasia045@gmail.com>
2020-08-20 17:28:51 +0900

    numlistの追加

---
+++ IoPractice/src/com/umemiya/lambdaPrc/StreamPrc.java
@@ -0,0 +1,12 @@
+package com.umemiya.lambdaPrc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamPrc {
+    public static void main(String[] args) {
+        List<Integer> numList = Arrays.asList(1, 2, 3);
+
+    }
+}
