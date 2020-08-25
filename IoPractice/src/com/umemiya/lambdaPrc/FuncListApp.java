d5c22abd3065b3e9d112f614788b0ca214fd2925
梅宮 <euthanasia045@gmail.com>
2020-08-25 18:14:28 +0900

    add FuncListApp

--- IoPractice/src/com/umemiya/lambdaPrc/FuncListApp.java
+++ IoPractice/src/com/umemiya/lambdaPrc/FuncListApp.java
@@ -3,6 +3,10 @@ package com.umemiya.lambdaPrc;
 public class FuncListApp {
     public static void main(String[] args) {
         FuncList funcList = new FuncList();
-
+        Func1 f1 = FuncList::isOdd;
+        // 非staticなのでインスタンスからじゃないと呼べない
+        Func2 f2 = funcList::addNamePrefix;
+        System.out.println(f1.call(3));
+        System.out.println(f2.call(true, "umemiya"));
     }
 }
