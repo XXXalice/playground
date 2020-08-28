b687782309124ded53ded21a66351c5afc15cec3
梅宮 <euthanasia045@gmail.com>
2020-08-28 17:27:03 +0900

    property

---
+++ IoPractice/src/com/umemiya/jvmHandler/PropertyPrc.java
@@ -0,0 +1,8 @@
+package com.umemiya.jvmHandler;
+
+public class PropertyPrc {
+    public static void main(String[] args) {
+        System.out.println("javaのばーじょん！！");
+        System.out.println(System.getProperty("java.version"));
+    }
+}
