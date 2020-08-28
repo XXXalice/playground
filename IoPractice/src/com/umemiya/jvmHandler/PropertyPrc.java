7deb51cded0e3870f2007ca20313a85c5fa4cfcf
梅宮 <euthanasia045@gmail.com>
2020-08-28 17:30:12 +0900

    add property

--- IoPractice/src/com/umemiya/jvmHandler/PropertyPrc.java
+++ IoPractice/src/com/umemiya/jvmHandler/PropertyPrc.java
@@ -1,8 +1,21 @@
 package com.umemiya.jvmHandler;
 
+import java.util.Iterator;
+import java.util.Properties;
+
 public class PropertyPrc {
     public static void main(String[] args) {
         System.out.println("javaのばーじょん！！");
         System.out.println(System.getProperty("java.version"));
+
+        // getPropertiesでMap型のプロパティ一覧が取れる
+        Properties p = System.getProperties();
+        Iterator<String> i = p.stringPropertyNames().iterator();
+        System.out.println("プロパティ一覧");
+        while(i.hasNext()) {
+            String key = i.next();
+            System.out.print(key + " = ");
+            System.out.println(System.getProperty(key));
+        }
     }
 }
